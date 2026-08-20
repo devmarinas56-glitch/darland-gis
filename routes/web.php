f5966d285ed2b747f14957d34868c26b0d79add1
devmarinas56-glitch <dev.marinas56@gmail.com>
2026-08-20 14:05:58 +0800

    Add GET /login route with name for login form

--- routes/web.php
+++ routes/web.php
@@ -82,6 +82,7 @@ Route::get('/debug-auth', function () {
 
     return response()->json($result);
 });
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
